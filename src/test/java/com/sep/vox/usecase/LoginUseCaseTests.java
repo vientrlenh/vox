a637064da0b09b630dd6761c1e1396c570aa2963
vientrlenh <tranlenhat123456@gmail.com>
2026-05-15 19:37:30 +0700

    fix test cases in login use case test

--- src/test/java/com/sep/vox/usecase/LoginUseCaseTests.java
+++ src/test/java/com/sep/vox/usecase/LoginUseCaseTests.java
@@ -1,6 +1,7 @@
 package com.sep.vox.usecase;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +27,6 @@ import com.sep.vox.domain.repository.UserRoleRepository;
 import com.sep.vox.domain.valueobject.Email;
 import com.sep.vox.domain.valueobject.Phone;
 
-import jakarta.persistence.EntityManager;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -47,9 +48,6 @@ public class LoginUseCaseTests {
 
     @Autowired
     private PasswordEncoderPort passwordEncoderPort;
-
-    @Autowired
-    private EntityManager entityManager;
 
     @Test
     void login_should_work() {
@@ -80,19 +78,59 @@ public class LoginUseCaseTests {
         );
 
         var savedUser = userRepository.save(activeUser);
-        entityManager.flush();
-        entityManager.clear();
 
         var userRole = new UserRole(savedUser.getId(), savedRole.getId(), OffsetDateTime.now());
 
         userRoleRepository.save(userRole);
-        entityManager.flush();
-        entityManager.clear();
         
         var result = loginUseCase.execute(new LoginCommand(
             "test@example.com", 
             "123456"));
         
         assertThat(result).isNotNull();
+    }
+
+    @Test
+    void login_should_reject() {
+        var saRole = new Role(
+            "SCHOOL_ADMIN",
+            "School admin",
+            OffsetDateTime.now(),
+            OffsetDateTime.now(),
+            null,
+            null
+        );
+
+        var savedRole = roleRepository.save(saRole);
+
+        var activeUser = new User(
+            new Email("test@example.com"),
+            passwordEncoderPort.hash("123456"),
+            new Phone("0987654321"),
+            "Test User",
+            null,
+            LocalDate.of(2000, 1, 1),
+            "Ho Chi Minh City",
+            UserStatus.ACTIVE,
+            OffsetDateTime.now(),
+            OffsetDateTime.now(),
+            null,
+            null
+        );
+
+        var savedUser = userRepository.save(activeUser);
+
+        var userRole = new UserRole(savedUser.getId(), savedRole.getId(), OffsetDateTime.now());
+
+        userRoleRepository.save(userRole);
+        
+        assertThrows(
+            AuthenticationException.class, 
+            () -> loginUseCase.execute(new LoginCommand(
+            "test@example.com", 
+            "12345"))
+        );
+        
+        
     }
 }

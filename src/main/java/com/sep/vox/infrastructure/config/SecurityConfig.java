aa5fc63512fcb7c2c4fc44c923df54ff835ca7fd
lenhat <tranlenhat123456@gmail.com>
2026-05-28 08:44:21 +0700

    add cors for dev frontend url

--- src/main/java/com/sep/vox/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/sep/vox/infrastructure/config/SecurityConfig.java
@@ -51,7 +51,9 @@ public class SecurityConfig {
         "/graphiql/**"
     };
 
-    private static final List<String> CORS_ALLOWED_PATTERNS = List.of();
+    private static final List<String> CORS_ALLOWED_PATTERNS = List.of(
+        "http://localhost:5173"
+    );
     private static final List<String> CORS_ALLOWED_METHODS = List.of(
         HttpMethod.GET.name(),
         HttpMethod.POST.name(),

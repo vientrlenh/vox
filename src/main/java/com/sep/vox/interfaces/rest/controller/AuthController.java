8aa7e9a377f57c2c6f2b71356155426f09a61320
hoangviet282004 <vietle282004@gmail.com>
2026-06-01 07:09:40 +0700

    Fix text

--- src/main/java/com/sep/vox/interfaces/rest/controller/AuthController.java
+++ src/main/java/com/sep/vox/interfaces/rest/controller/AuthController.java
@@ -1,11 +1,8 @@
 package com.sep.vox.interfaces.rest.controller;
 
-import com.sep.vox.application.port.input.command.LoginGoogleCommand;
 import com.sep.vox.application.port.input.usecase.auth.*;
 import com.sep.vox.interfaces.rest.dto.request.*;
 import com.sep.vox.interfaces.rest.mapper.*;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -34,16 +31,14 @@ public class AuthController {
     private final RefreshUseCase refreshUseCase;
     private final SendResetPasswordOtpUseCase sendResetPasswordOtpUseCase;
     private final ResetPasswordUseCase resetPasswordUseCase;
-    private final LoginGoogleUseCase loginGoogleUseCase;
 
-    public AuthController(LoginUseCase loginUseCase, RegisterUseCase registerUseCase, SetUpPasswordUseCase setUpPasswordUseCase, RefreshUseCase refreshUseCase, SendResetPasswordOtpUseCase sendResetPasswordOtpUseCase, ResetPasswordUseCase resetPasswordUseCase, LoginGoogleUseCase loginGoogleUseCase) {
+    public AuthController(LoginUseCase loginUseCase, RegisterUseCase registerUseCase, SetUpPasswordUseCase setUpPasswordUseCase, RefreshUseCase refreshUseCase, SendResetPasswordOtpUseCase sendResetPasswordOtpUseCase, ResetPasswordUseCase resetPasswordUseCase) {
         this.loginUseCase = loginUseCase;
         this.registerUseCase = registerUseCase;
         this.setUpPasswordUseCase = setUpPasswordUseCase;
         this.refreshUseCase = refreshUseCase;
         this.sendResetPasswordOtpUseCase = sendResetPasswordOtpUseCase;
         this.resetPasswordUseCase = resetPasswordUseCase;
-        this.loginGoogleUseCase = loginGoogleUseCase;
     }
 
     @PostMapping("/login")
@@ -98,30 +93,5 @@ public class AuthController {
         resetPasswordUseCase.execute(command);
         var response = ApiResponse.success("Mật khẩu đã thay đổi thành công");
         return ResponseEntity.ok(response);
-    }
-
-
-    // ====LOGIN Google==============
-    @Operation(
-            summary = "Đăng nhập bằng Google",
-            description = "Xác thực idToken từ Google và cấp Access Token & Session Token cho hệ thống VOX"
-    )
-    @PostMapping("/google")
-    public ResponseEntity<ApiResponse<LoginResponse>> loginWithGoogle(
-            @Valid @RequestBody LoginGoogleRequest request,
-            HttpServletRequest httpRequest) {
-
-        // 1. Trích xuất thông tin mạng
-        String ipAddress = IpAddressReceiver.getClientIp(httpRequest);
-        String userAgent = httpRequest.getHeader(org.springframework.http.HttpHeaders.USER_AGENT);
-
-        // 2. Đóng gói Command thông qua Mapper (Controller giờ rất gọn gàng)
-        var command = LoginGoogleCommandMapper.fromRequest(request, ipAddress, userAgent);
-
-        // 3. Thực thi Use Case
-        LoginResponse response = loginGoogleUseCase.execute(command);
-
-        // 4. Trả về Response
-        return ResponseEntity.ok(ApiResponse.success(String.valueOf(response)));
     }
 }
---
+++ src/main/java/com/sep/vox/interfaces/rest/controller/LoginGoogleController.java
@@ -0,0 +1,53 @@
+package com.sep.vox.interfaces.rest.controller;
+
+
+import com.sep.vox.application.port.input.usecase.auth.LoginGoogleUseCase;
+import com.sep.vox.application.response.input.auth.LoginResponse;
+import com.sep.vox.interfaces.rest.dto.request.LoginGoogleRequest;
+import com.sep.vox.interfaces.rest.dto.response.ApiResponse;
+import com.sep.vox.interfaces.rest.mapper.LoginGoogleCommandMapper;
+import com.sep.vox.interfaces.shared.IpAddressReceiver;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class LoginGoogleController {
+
+    private final LoginGoogleUseCase loginGoogleUseCase;
+
+    public LoginGoogleController(LoginGoogleUseCase loginGoogleUseCase) {
+        this.loginGoogleUseCase = loginGoogleUseCase;
+    }
+
+
+    // ====LOGIN Google==============
+    @Operation(
+            summary = "Đăng nhập bằng Google",
+            description = "Xác thực idToken từ Google và cấp Access Token & Session Token cho hệ thống VOX"
+    )
+    @PostMapping("/google")
+    public ResponseEntity<ApiResponse<LoginResponse>> loginWithGoogle(
+            @Valid @RequestBody LoginGoogleRequest request,
+            HttpServletRequest httpRequest) {
+
+        // 1. Trích xuất thông tin mạng
+        String ipAddress = IpAddressReceiver.getClientIp(httpRequest);
+        String userAgent = httpRequest.getHeader(org.springframework.http.HttpHeaders.USER_AGENT);
+
+        // 2. Đóng gói Command thông qua Mapper (Controller giờ rất gọn gàng)
+        var command = LoginGoogleCommandMapper.fromRequest(request, ipAddress, userAgent);
+
+        // 3. Thực thi Use Case
+        LoginResponse response = loginGoogleUseCase.execute(command);
+
+        // 4. Trả về Response
+        return ResponseEntity.ok(ApiResponse.success(String.valueOf(response)));
+    }
+}

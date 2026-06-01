3db8780f57a00e32d23994b4a0a4d181d2565f88
vientrlenh <tranlenhat123456@gmail.com>
2026-06-01 22:47:50 +0700

    fix test

--- src/test/java/com/sep/vox/VoxApplicationTests.java
+++ src/test/java/com/sep/vox/VoxApplicationTests.java
@@ -2,10 +2,14 @@ package com.sep.vox;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.sep.vox.config.TestContainerConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestContainerConfig.class)
 class VoxApplicationTests {
 
 	@Test

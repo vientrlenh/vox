68a0aec3b1737e73676c9543444f823843156e52
vientrlenh <tranlenhat123456@gmail.com>
2026-06-23 19:34:59 +0700

    add documents attribute in graphql register form type

--- src/main/java/com/sep/vox/interfaces/graphql/controller/RegisterFormController.java
+++ src/main/java/com/sep/vox/interfaces/graphql/controller/RegisterFormController.java
@@ -1,10 +1,14 @@
 package com.sep.vox.interfaces.graphql.controller;
 
 
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
+import org.dataloader.DataLoader;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
@@ -13,7 +17,10 @@ import com.sep.vox.application.port.input.query.ViewRegisterFormsQuery;
 import com.sep.vox.application.port.input.usecase.registration.ViewRegisterFormDetailsUseCase;
 import com.sep.vox.application.port.input.usecase.registration.ViewRegisterFormsUseCase;
 import com.sep.vox.domain.common.PageResult;
+import com.sep.vox.domain.dto.RegisterFormDocumentDto;
 import com.sep.vox.domain.dto.RegisterFormDto;
+
+import graphql.schema.DataFetchingEnvironment;
 
 @Controller("graphqlRegisterFormController")
 public class RegisterFormController {
@@ -42,4 +49,13 @@ public class RegisterFormController {
         var query = new ViewRegisterFormDetailsQuery(id);
         return viewRegisterFormDetailsUseCase.execute(query);
     }
+
+    @SchemaMapping(typeName = "RegisterForm", field = "documents")
+    public CompletableFuture<List<RegisterFormDocumentDto>> registerFormDocuments(RegisterFormDto form, DataFetchingEnvironment env) {
+        DataLoader<UUID, List<RegisterFormDocumentDto>> loader = env.getDataLoader("documentsByRegisterForm");
+        if (loader == null) {
+            throw new IllegalStateException("Không tìm thấy loader của documentsByRegisterForm");
+        }
+        return loader.load(form.id());
+    } 
 }

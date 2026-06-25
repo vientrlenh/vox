fc6e1bd72dda7c367fa7ae7483a3684a90179687
vientrlenh <tranlenhat123456@gmail.com>
2026-06-25 23:18:54 +0700

    add grpc setup

--- src/main/java/com/sep/vox/VoxApplication.java
+++ src/main/java/com/sep/vox/VoxApplication.java
@@ -2,7 +2,9 @@ package com.sep.vox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.grpc.client.ImportGrpcClients;
 
+@ImportGrpcClients(basePackageClasses = VoxApplication.class)
 @SpringBootApplication
 public class VoxApplication {
 

c4f2915ecc7f7e5bf82d8f1e88cc94b530fccc17
vientrlenh <tranlenhat123456@gmail.com>
2026-07-01 20:17:39 +0700

    add auto configure grpc test

--- src/test/java/com/sep/vox/VoxApplicationTests.java
+++ src/test/java/com/sep/vox/VoxApplicationTests.java
@@ -1,6 +1,7 @@
 package com.sep.vox;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.grpc.test.autoconfigure.AutoConfigureTestGrpcTransport;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,6 +10,7 @@ import com.sep.vox.config.ContainerTestConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestGrpcTransport
 class VoxApplicationTests extends ContainerTestConfig {
 
 	@Test

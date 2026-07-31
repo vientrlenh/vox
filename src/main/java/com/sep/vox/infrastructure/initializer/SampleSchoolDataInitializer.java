29c71ce3da585d0325caa245369282bc7816d4b0
vientrlenh <tranlenhat123456@gmail.com>
2026-07-31 11:03:03 +0700

    remove chrono file import in sample school data initializer

--- src/main/java/com/sep/vox/infrastructure/initializer/SampleSchoolDataInitializer.java
+++ src/main/java/com/sep/vox/infrastructure/initializer/SampleSchoolDataInitializer.java
@@ -3,7 +3,6 @@ package com.sep.vox.infrastructure.initializer;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
